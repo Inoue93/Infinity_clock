705ca4e3b3821254db2ce12570be4253d1fb1837
Angelika <44729890+Inoue93@users.noreply.github.com>
2021-08-30 12:57:09 +0200

    Update projekt.ino

--- projekt.ino
+++ projekt.ino
@@ -83,25 +83,25 @@ void animation()
   
  }
  // colorWipe animation - to understand....
- void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
+ void colorWipe(uint32_t c, uint8_t wait) {     // c = color of leds
+  for(uint16_t i=0; i<strip.numPixels(); i++) {    // t = time 
     strip.setPixelColor(i, c);
     strip.show();
     delay(wait);
   }
 }
 //theaterChade animation - to understand....
-  void theaterChase(uint32_t c, uint8_t wait) {
+  void theaterChase(uint32_t c, uint8_t wait) {         // c = color of leds
   for (int j=0; j<10; j++) {  //do 10 cycles of chasing
     for (int q=0; q < 3; q++) {
       for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
+        strip.setPixelColor(i+q, c);    //turn every third pixel (q)
       }
       strip.show();
 
       delay(wait);
 
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {        // t = time 
         strip.setPixelColor(i+q, 0);        //turn every third pixel off
       }
     }
